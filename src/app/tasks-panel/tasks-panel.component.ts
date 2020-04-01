98e99b32c483ab908a19da96727354a0c810c2d1
Anthony Perez <antrp93@gmail.com>
2020-03-31 23:12:29 -0500

    Added dynamic data to TaskPanel component

--- src/app/tasks-panel/tasks-panel.component.ts
+++ src/app/tasks-panel/tasks-panel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import tasks,{} from '../tasks';
 
 @Component({
   selector: 'app-tasks-panel',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TasksPanelComponent implements OnInit {
 
+  tasks;
   constructor() { }
 
   ngOnInit() {
+    this.tasks = tasks;
   }
 
 }
